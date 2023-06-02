b1a5845c4793b952669cb4721ad27e521744cfc0
Edu <121562691+EduardoACalvo@users.noreply.github.com>
2023-06-02 06:26:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 from flask import Flask, render_template, request
 import random
 
-
 app = Flask(__name__)
 
 @app.route('/')
-def raiz():
+def root():
+  return "Hola mundo!"
+
+@app.route('/r-p-s')
+def rock_paper_scissors():
   game = ['Rock', 'Paper', 'Scissors']
   player1= random.choice(game)
   player2= random.choice(game)
 
-  return render_template('index.html', pl1=player1, pl2=player2)
-  
-
-
+  return render_template('r-p-s.html', pl1=player1, pl2=player2)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port='80',debug='')
-
-
-  #curl "https://api.open-meteo.com/v1/forecast?latitude=-38.72&longitude=-62.27&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m"
+    app.run(host='0.0.0.0',port='80',debug='on')
